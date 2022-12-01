e350beed7227543c2977714550b1c0c2607efa1b
Zachary Stence <zachstence@gmail.com>
2022-12-01 08:33:22 -0600

    feat(day1): part1 complete

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,7 +1,25 @@
 use std::fs;
+use std::cmp;
 
-pub fn part1(input_file: &str) {
-    let input = fs::read_to_string(input_file)
-        .expect("Should be able to read input file");
-    println!("{input}")
+pub fn part1(input_file: &str) -> i32 {
+    let input = fs::read_to_string(input_file).unwrap();
+    
+    let tokens = input.split("\n");
+
+    let mut max_calories = 0;
+    let mut calories = 0;
+
+    for token in tokens {
+        // Empty line means we're moving to the next elf, so save current calories as max
+        // Non-empty line means we're adding calories
+        if token.is_empty() {
+            max_calories = cmp::max(max_calories, calories);
+            calories = 0;
+        } else {
+            let c = token.parse::<i32>().unwrap();
+            calories += c;
+        }
+    }
+
+    return max_calories;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 
 fn main() {
-    day1::part1("./src/day1/test.txt");
+    let answer = day1::part1("./src/day1/input.txt");
+    println!("{answer}")
 }
