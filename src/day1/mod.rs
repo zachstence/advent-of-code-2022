ecad68908d18f5fad3d646d5867bae6de45ddac2
Zachary Stence <zachstence@gmail.com>
2022-12-01 08:26:54 -0600

    feat(day1): read input file

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,3 +1,7 @@
-pub fn part1() {
-    println!("day1 part1")
+use std::fs;
+
+pub fn part1(input_file: &str) {
+    let input = fs::read_to_string(input_file)
+        .expect("Should be able to read input file");
+    println!("{input}")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod day1;
 
 fn main() {
-    day1::part1();
+    day1::part1("./src/day1/test.txt");
 }
