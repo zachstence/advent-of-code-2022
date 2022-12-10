261552105ab935ef6f0cf2721cf4e1eb2ef7b9df
Zachary Stence <zachstence@gmail.com>
2022-12-10 13:49:11 -0600

    feat(day9): part 2 complete

--- src/day9.rs
+++ src/day9.rs
@@ -5,12 +5,25 @@ type Point = (i32, i32);
 
 #[aoc(day9, part1)]
 pub fn part1(input: &str) -> usize {
+    simulate_rope(input, 2)
+}
+
+#[aoc(day9, part2)]
+pub fn part2(input: &str) -> usize {
+    simulate_rope(input, 10)
+}
+
+
+//////////////
+
+
+fn simulate_rope(input: &str, rope_length: usize) -> usize {
     let mut visited: HashSet<Point> = HashSet::new();
 
-    let mut head: Point = (0, 0);
-    let mut tail: Point = (0, 0);
+    let mut knots: Vec<Point> = vec![(0, 0); rope_length];
 
-    visited.insert(tail);
+    // All knots start at (0, 0), so it is visisted
+    visited.insert((0, 0));
 
     input
         .lines()
@@ -20,10 +33,14 @@ pub fn part1(input: &str) -> usize {
 
             #[allow(clippy::single_element_loop)]
             for _ in 0..distance {
-                update_positions(direction, &mut head, &mut tail);
-                visited.insert(tail);
+                update_positions(direction, &mut knots);
+
+                // Track position of tail knot (last in array)
+                visited.insert(*knots.last().unwrap());
             }
+
         });
+
 
     visited.len()
 }
@@ -44,59 +61,70 @@ fn move_point(direction: &str, p: &mut Point) {
     }
 }
 
-fn update_positions(direction: &str, head: &mut Point, tail: &mut Point) {
+fn update_positions(direction: &str, knots: &mut [Point]) {
+    let head = knots.get_mut(0).unwrap();
+
     // First move head
     move_point(direction, head);
 
-    // Move tail to catch up
-    let (dx, dy) = delta(head, tail);
+    // Move other knots to catch up
+    let mut prev = *head;
 
-    match (dx, dy) {
-        // Tail is in the same spot as head
-        // No move necessary
-        (0, 0) => (),
+    for knot in knots[1..].iter_mut() {
 
-        // Tail is adjacent to head and in the same row/column
-        // No move necessary
-        (0, -1) | (0, 1) | (-1, 0) | (1, 0) => (),
-        
-        // Tail is adjacent to head and catty-corner
-        // No move necessary
-        (-1, -1) | (-1, 1) | (1, -1) | (1, 1) => (),
+        let (dx, dy) = delta(&prev, knot);
 
-        // Tail is not adjacent, but in the same row column
-        // To catch up, we move the same direction that head moved
-        (0, 2) | (0, -2) | (2, 0) | (-2, 0) => move_point(direction, tail),
+        match (dx, dy) {
+            // Tail is in the same spot as head
+            // No move necessary
+            (0, 0) => (),
+    
+            // Tail is adjacent to head and in the same row/column
+            // No move necessary
+            (0, -1) | (0, 1) | (-1, 0) | (1, 0) => (),
+            
+            // Tail is adjacent to head and catty-corner
+            // No move necessary
+            (-1, -1) | (-1, 1) | (1, -1) | (1, 1) => (),
+    
+            // Tail is not adjacent, but in the same row column
+            // To catch up, we move URDL
+            (0, 2) => move_point("U", knot),
+            (2, 0) => move_point("R", knot),
+            (0, -2) => move_point("D", knot),
+            (-2, 0) => move_point("L", knot),
+    
+            // The rest of this `match` means tail needs to move diagonally to catch up
+    
+            // UR
+            (1, 2) | (2, 1) | (2, 2) => {
+                move_point("U", knot);
+                move_point("R", knot);
+            },
+    
+            // UL
+            (-1, 2) | (-2, 1) | (-2, 2) => {
+                move_point("U", knot);
+                move_point("L", knot);
+            }
+    
+            // DL
+            (-1, -2) | (-2, -1) | (-2, -2) => {
+                move_point("D", knot);
+                move_point("L", knot);
+            },
+    
+            // DR
+            (1, -2) | (2, -1) | (2, -2) => {
+                move_point("D", knot);
+                move_point("R", knot);
+            },
+    
+            _ => panic!("Tail is too far away from head! (dx={}, dy={})", dx, dy)
+        };
 
-        // The rest of this `match` means tail needs to move diagonally to catch up
-
-        // UR
-        (1, 2) | (2, 1) => {
-            move_point("U", tail);
-            move_point("R", tail);
-        },
-
-        // UL
-        (-1, 2) | (-2, 1) => {
-            move_point("U", tail);
-            move_point("L", tail);
-        }
-
-        // DL
-        (-1, -2) | (-2, -1) => {
-            move_point("D", tail);
-            move_point("L", tail);
-        },
-
-        // DR
-        (1, -2) | (2, -1) => {
-            move_point("D", tail);
-            move_point("R", tail);
-        },
-
-        _ => panic!("Tail is too far away from head! (dx={}, dy={})", dx, dy)
-    };
-
+        prev = *knot;
+    }
 }
 
 
@@ -105,11 +133,27 @@ mod day9_tests {
     use super::*;
 
     #[test]
-    fn sample_input() {
+    fn part1_sample_input() {
         let input = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2";
 
         let answer = part1(input);
         assert_eq!(answer, 13);
+    }
+
+    #[test]
+    fn part2_sample_input() {
+        let input = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2";
+
+        let answer = part2(input);
+        assert_eq!(answer, 1);
+    }
+
+    #[test]
+    fn part2_larger_sample_input() {
+        let input = "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20";
+
+        let answer = part2(input);
+        assert_eq!(answer, 36);
     }
 
     /**
