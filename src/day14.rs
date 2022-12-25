89a412c3de507548c81f6153043b4ba59d45d79e
Zachary Stence <zachstence@gmail.com>
2022-12-25 17:39:23 -0600

    feat(day14): part 1 complete

--- src/day14.rs
+++ src/day14.rs
@@ -3,7 +3,8 @@ use std::{collections::HashSet};
 use itertools::Itertools;
 
 const SAND_DROP: Point = (500, 0);
-const MAX_ITERS: usize = 1;
+const DX: &[i32] = &[0, -1, 1];
+const MAX_SAND: usize = 10_000;
 
 #[aoc(day14, part1)]
 pub fn part1(input: &str) -> usize {
@@ -20,7 +21,7 @@ pub fn part1(input: &str) -> usize {
             line
                 .split(" -> ")
                 .map(|token| token.split_once(',').unwrap())
-                .map(|(x, y)| (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap()))
+                .map(|(x, y)| (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap()))
                 .tuple_windows()
                 .for_each(|((start_x, start_y), (end_x, end_y))| {
                     min_x = min_x.min(start_x).min(end_x);
@@ -48,16 +49,29 @@ pub fn part1(input: &str) -> usize {
     
     // Rocks and Sand could be kept in the same HashSet, but having them separate allows us to display them
     let mut sand: HashSet<Point> = HashSet::new();
-    display(&rocks, &sand, &(min_x, min_y), &(max_x, max_y));
 
-    for _ in 0..MAX_ITERS {
+    // Prevent infinite loop from a bug
+    for _ in 0..MAX_SAND {
         let mut x = SAND_DROP.0;
-        for y in SAND_DROP.1..=max_y {
-            
+        let mut y = SAND_DROP.1;
+        while y != max_y {
+            if let Some(dx) = DX.iter().find(|dx| !is_occupied(&rocks, &sand, &(x + *dx, y + 1))) {
+                // Falls to (x,y)
+                x += *dx;
+                y += 1;
+                continue;
+            } else {
+                // Can't fall, is at rest
+                sand.insert((x, y));
+                break;
+            }
         }
+
+        // Sand has fallen to the abyss, we're done simulating
+        if y == max_y { break; }
     }
 
-    0
+    sand.len()
 }
 
 // #[aoc(day14, part2)]
@@ -65,18 +79,18 @@ pub fn part1(input: &str) -> usize {
 //     0
 // }
 
-type Point = (usize, usize);
+type Point = (i32, i32);
 
 fn display(rocks: &HashSet<Point>, sand: &HashSet<Point>, min: &Point, max: &Point) {
-    println!("  {:03}{}{:03}", min.0, " ".repeat(max.0 - min.0 - 1), max.0);
+    println!("  {:03}{}{:03}", min.0 - 1, " ".repeat((max.0 - min.0 + 1) as usize), max.0 + 1);
 
-    for y in min.1..=max.1 {
+    for y in min.1..=max.1 + 1 {
         print!("{y:03} ");
-        for x in min.0..=max.0 {
+        for x in min.0 - 1..=max.0 + 1 {
             if rocks.contains(&(x, y)) {
                 print!("#");
             } else if sand.contains(&(x, y)) {
-                print!("O");
+                print!("o");
             } else if (x, y) == SAND_DROP {
                 print!("+");
             } else {
@@ -85,6 +99,10 @@ fn display(rocks: &HashSet<Point>, sand: &HashSet<Point>, min: &Point, max: &Poi
         }
         println!();
     }
+}
+
+fn is_occupied(rocks: &HashSet<Point>, sand: &HashSet<Point>, point: &Point) -> bool {
+    rocks.contains(point) || sand.contains(point)
 }
 
 #[cfg(test)]
@@ -99,7 +117,7 @@ mod day14_tests {
     #[test]
     fn part1_sample_input() {
         let answer = part1(SAMPLE_INPUT);
-        assert_eq!(answer, 0);
+        assert_eq!(answer, 24);
     }
 
     // #[test]
